6f9f28e869d1c1e97d6f126be42cdec3af0702ec
iv-tsim <iv-tsim@yandex.ru>
2021-01-26 18:00:04 +0300

    added many pages

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
 	objectFitImages();
 
+	const slidersTabs = document.querySelectorAll('.portfolio__slider');
+
+
 	const slider_pag = new Swiper('.pag-hero__slider', {
 		slidesPerView: 'auto',
 		spaceBetween: 45,
@@ -46,15 +49,57 @@ $(document).ready(function () {
 	// slider_pag.controller.control = [slider_hero];
 
 	const slider2 = new Swiper('.fourth__slider', {
-		slidesPerView: 3,
+		slidesPerView: 4,
 		loop: false,
 		spaceBetween: 40,
+		navigation: {
+			nextEl: '.fourth__next',
+			prevEl: '.fourth__prev',
+		},
 	});
 
 	$('.item-third__top').on('click', function (ev) {
 		let text = $(this).next('.item-third__bot');
 		$(this).toggleClass('item-third__top--active')
 		text.slideToggle();
+	})
+
+	$('.price-item__top').on('click', function (ev) {
+		let text = $(this).next('.price-item__bot');
+		$(this).toggleClass('price-item__top--active')
+		text.slideToggle();
+	})
+
+	slidersTabs.forEach(function (el) {
+
+		const slider_1 = new Swiper(el, {
+
+			slidesPerView: 2,
+			spaceBetween: 31,
+			observer: true,
+			observeParents: true,
+			observeSlideChildren: true,
+			loop: false,
+			// navigation: {
+			// 	nextEl: el.closest('.slider-1').querySelector('.slider-1__next'),
+			// 	prevEl: el.closest('.slider-1').querySelector('.slider-1__prev'),
+			// },
+			// breakpoints: {
+			// 	320: {
+			// 		slidesPerView: 1,
+			// 	},
+			// 	653: {
+			// 		slidesPerView: 2,
+			// 	},
+			// 	1130: {
+			// 		slidesPerView: 3,
+			// 	},
+			// 	1340: {
+			// 		slidesPerView: 4,
+			// 		spaceBetween: 30,
+			// 	}
+			// }
+		});
 	})
 
 	// Fancy-box
@@ -79,11 +124,11 @@ $(document).ready(function () {
 	// Input-mask
 	// $('input[type="tel"]').inputmask({ "mask": "+7 (999)-999-99-99" });
 
-	// // Табы
-	// $('ul.tabs__list').on('click', 'li:not(.active)', function () {
-	// 	$(this).addClass('active').siblings().removeClass('active')
-	// 		.closest('div.tabs').find('div.tabs__block').removeClass('active').eq($(this).index()).addClass('active');
-	// })
+	// Табы
+	$('ul.portfolio__tabs').on('click', 'li:not(.active)', function () {
+		$(this).addClass('active').siblings().removeClass('active')
+			.closest('div.portfolio__flex').find('div.portfolio__tabs-block').removeClass('active').eq($(this).index()).addClass('active');
+	})
 
 	// Menu-burger
 	// burger.click(function () {
